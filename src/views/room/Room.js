f2e57c4d15da0fd68f15a3e08da9226ce25c64a7
Sziklai Dávid <szikla12david@gmail.com>
2020-05-05 17:12:31 +0200

    try

--- src/views/room/Room.js
+++ src/views/room/Room.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export function Room() {
     return (
         <>
-            <h1 className="text-light">Várakozó szoba: 1</h1>
+            <h1 className="text-light">Várakozó szoba: 2</h1>
             <h2 className="text-light">Váralozás másik játékosra...</h2>
             <Link to="/" className="btn btn-outline-primary m-3">Főoldal</Link>
         </>
